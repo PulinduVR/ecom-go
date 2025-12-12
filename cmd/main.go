e259d9512356de3767f5de59522294278bb7f5f1
PulinduVR <pulinduv@gmail.com>
2025-12-12 12:51:41 +0530

    feat(db): Migrations completed

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,38 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/PulinduVR/ecom-go/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
 	config := config{
 		addr: ":8000",
-		db:   dbConfig{},
-	}
-
-	api := application{
-		config: config,
+		db: dbConfig{
+			dbn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=ecom sslmode=disable"),
+		},
 	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	slog.SetDefault(logger)
+
+	//Database
+	conn, err := pgx.Connect(ctx, config.db.dbn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+	logger.Info("Database Connected", "dbn", config.db.dbn)
+
+	api := application{
+		config: config,
+	}
 
 	// h := api.mount()
 	if err := api.run(api.mount()); err != nil {
